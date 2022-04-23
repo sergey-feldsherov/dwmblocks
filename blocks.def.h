032cf30765b15800975f1cbe108927b4c6fdcd1e
sergey-feldsherov <imnosuperman74@gmail.com>
2022-04-23 08:31:49 +0300

    First working version (sort of) with cpu temp, fan rpm, ram, battery, volume and date.

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" CPU ", "sensors | awk '/^CPU/ {print $2}'",                      5,     1},
+	{"FAN ", "sensors | awk '/^fan1/ {print $2 $3}'",                   5,     2},
+	{"RAM ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 10,    3},
+	{"BAT ", "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/ {print $2}'", 30, 3},
+	{"VOL ", "pamixer --get-volume-human",                              86400, 4},
+	{"",      "date '+%A %d %b %Y %I:%M%p '",                           60,    5},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
