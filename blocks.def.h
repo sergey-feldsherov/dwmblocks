dd91e5ce8bf02a30be29bb15f34e6120ae48325e
sergey-feldsherov <imnosuperman74@gmail.com>
2022-04-26 12:15:51 +0300

    Introduce screen brightness display

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" CPU ", "sensors | awk '/^CPU/ {print $2}'",                                                      5,     1},
-	{"FAN ",  "sensors | awk '/^fan1/ {print $2 $3}'",                                                  5,     2},
-	{"RAM ",  "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",                                10,    3},
-	{"BAT ",  "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/ {print $2}'", 30,    3},
-	{"VOL ",  "pamixer --get-volume-human",                                                             86400, 4},
-	{"",      "date '+%A %d %b %Y %I:%M%p '",                                                           60,    5},
+	{" CPU ", "sensors | awk '/^CPU/ {print $2}'",                                                      5,  1},
+	{"FAN ",  "sensors | awk '/^fan1/ {print $2 $3}'",                                                  5,  2},
+	{"RAM ",  "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",                                10, 3},
+	{"BRI ",  "echo \"$(brillo -G)%\"",                                                                 0,  7},
+	{"BAT ",  "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/ {print $2}'", 30, 4},
+	{"VOL ",  "pamixer --get-volume-human",                                                             0,  5},
+	{"",      "date '+%A %d %b %Y %I:%M%p '",                                                           60, 6},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
