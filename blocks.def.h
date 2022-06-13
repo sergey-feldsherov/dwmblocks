8329ee2a167db05ce6b0d0f204f163ffd12540bf
sergey-feldsherov <imnosuperman74@gmail.com>
2022-06-14 00:14:11 +0500

    Updated time format

--- blocks.def.h
+++ blocks.def.h
@@ -7,7 +7,7 @@ static const Block blocks[] = {
 	{"BRI ",  "echo \"$(brillo -G)%\"",                                                                 0,  7},
 	{"BAT ",  "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/ {print $2}'", 30, 4},
 	{"VOL ",  "pamixer --get-volume-human",                                                             0,  5},
-	{"",      "date '+%A %d %b %Y %I:%M%p '",                                                           60, 6},
+	{"",      "date '+%A %d %b %Y %H:%M '",                                                           60, 6},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
